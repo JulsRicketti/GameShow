2afd986e0324c9c632be7372d2a98b7a2a44dfbc
Julia Ricketti <julsricketti@gmail.com>
2017-07-03 10:51:07 -0400

    feat(questions): added the question creation mechanism

--- frontend/src/components/QuestionForm.js
+++ frontend/src/components/QuestionForm.js
@@ -1,15 +1,40 @@
 import React from 'react'
+import { questionsService } from '../app'
 import { Button, Form } from 'semantic-ui-react'
 
-export default class QuestionForm extends React.Component {
-    render(){
-    const categories = [
+const categories = [
         {key:'h', text: 'History', value: 'history'},
         {key:'g', text: 'Geography', value: 'geography'},
         {key:'p', text: 'Programming', value: 'programming'},
         {key:'e', text: 'Entertainment', value: 'entertainment'},
         {key:'m', text: 'Misc', value: 'misc'},
     ]
+
+export default class QuestionForm extends React.Component {
+    
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            question: '',
+            answer: '',
+            category: ''
+        }
+    }
+
+    handleSubmit(evt){
+        const { question, answer, category } = this.state
+        questionsService.create({
+            question: question,
+            answer: answer,
+            category
+        })
+        .then(()=>{
+            console.log('question successfully created!')
+        })
+    }
+
+    render(){
         return (
             <Form>
                 <Form.Select options={categories} placeholder='Categories'/>
@@ -17,7 +42,7 @@ export default class QuestionForm extends React.Component {
                     <Form.Input label='Question'/>
                     <Form.Input label='Correct Answer' />
                 </Form.Group>
-                <Button type='submit'> Submit </Button>
+                <Button type='submit' onClick={(evt) => this.handleSubmit(evt)}> Submit </Button>
             </Form>
         )
     }
